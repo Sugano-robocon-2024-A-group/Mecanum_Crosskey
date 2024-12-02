b8088ec0b21af3dcde6bc237b9092abd31f950d7
1X21B148 <5571fujiue_oji@fuji.waseda.jp>
2024-12-02 20:57:57 +0900

    変数を配列型にした

--- Mecanum_crosskey.ino
+++ Mecanum_crosskey.ino
@@ -1,125 +1,59 @@
 #include <Arduino.h>
 #include <PS4Controller.h>
 
-// 使うピンの定義
-// 左前車輪
-const int LEFT_FRONT_IN1 = 18;
-const int LEFT_FRONT_IN2 = 19;
-const int LEFT_FRONT_PWM = 21;
-
-// 左後車輪
-const int LEFT_BACK_IN1 = 27;
-const int LEFT_BACK_IN2 = 14;
-const int LEFT_BACK_PWM = 13;
-
-// 右前車輪
-const int RIGHT_FRONT_IN1 = 22;
-const int RIGHT_FRONT_IN2 = 23;
-const int RIGHT_FRONT_PWM = 16;
-
-// 右後車輪
-const int RIGHT_BACK_IN1 = 32;
-const int RIGHT_BACK_IN2 = 33;
-const int RIGHT_BACK_PWM = 4;
+// モーターとエンコーダのピン設定 （左前　左後　右前　右後）
+const int motor_pwm[4] = {21, 13, 16, 4}; // PWM出力ピン
+const int motor_dir1[4] = {18, 27, 22, 32}; // 方向ピン1
+const int motor_dir2[4] = {19, 14, 23, 33}; // 方向ピン2
 
 const int LEDC_TIMER_BIT = 8;   // PWMの範囲(8bitなら0〜255、10bitなら0〜1023)
 const int LEDC_BASE_FREQ = 490; // 周波数(Hz)
 
 void init_pwm_setup()
 {
-  // 各ピンを出力モードに設定
-  pinMode(LEFT_FRONT_IN1, OUTPUT);
-  pinMode(LEFT_FRONT_IN2, OUTPUT);
-  pinMode(LEFT_BACK_IN1, OUTPUT);
-  pinMode(LEFT_BACK_IN2, OUTPUT);
-  pinMode(RIGHT_FRONT_IN1, OUTPUT);
-  pinMode(RIGHT_FRONT_IN2, OUTPUT);
-  pinMode(RIGHT_BACK_IN1, OUTPUT);
-  pinMode(RIGHT_BACK_IN2, OUTPUT);
-
-  // 各PWMピンをアタッチ
-  ledcAttach(LEFT_FRONT_PWM, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
-  ledcAttach(LEFT_BACK_PWM, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
-  ledcAttach(RIGHT_FRONT_PWM, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
-  ledcAttach(RIGHT_BACK_PWM, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  for (int i = 0; i < 4; i++)
+  {
+    // モーターピンを出力に設定
+    pinMode(motor_pwm[i], OUTPUT);
+    pinMode(motor_dir1[i], OUTPUT);
+    pinMode(motor_dir2[i], OUTPUT);
+    ledcAttach(motor_pwm[i], LEDC_BASE_FREQ, LEDC_TIMER_BIT); // PWM出力ピン、周波数490Hz、8ビット解像度
+  }
 }
 
-// 左前車輪の制御
-void frontLeftMotor(double pwm)
+// モーターの駆動方向を制御
+void driveMotor(int index, float controlSignal)
 {
-  if (pwm >= 0)
+  if (controlSignal >= 0)
   {
-    digitalWrite(LEFT_FRONT_IN1, HIGH);
-    digitalWrite(LEFT_FRONT_IN2, LOW);
+    digitalWrite(motor_dir1[index], HIGH);
+    digitalWrite(motor_dir2[index], LOW);
   }
   else
   {
-    digitalWrite(LEFT_FRONT_IN1, LOW);
-    digitalWrite(LEFT_FRONT_IN2, HIGH);
-    pwm = -pwm;
+    digitalWrite(motor_dir1[index], LOW);
+    digitalWrite(motor_dir2[index], HIGH);
+    controlSignal = -controlSignal;
   }
-  ledcWrite(LEFT_FRONT_PWM, pwm);
+  ledcWrite(motor_pwm[index], (int)controlSignal);
 }
 
-// 左後車輪の制御
-void backLeftMotor(double pwm)
+// モーターを停止
+void stopMotors()
 {
-  if (pwm >= 0)
+  for (int i = 0; i < 4; i++)
   {
-    digitalWrite(LEFT_BACK_IN1, HIGH);
-    digitalWrite(LEFT_BACK_IN2, LOW);
+    ledcWrite(motor_pwm[i], 0);
+    digitalWrite(motor_dir1[i], LOW);
+    digitalWrite(motor_dir2[i], LOW);
   }
-  else
-  {
-    digitalWrite(LEFT_BACK_IN1, LOW);
-    digitalWrite(LEFT_BACK_IN2, HIGH);
-    pwm = -pwm;
-  }
-  ledcWrite(LEFT_BACK_PWM, pwm);
-}
-
-// 右前車輪の制御
-void frontRightMotor(double pwm)
-{
-  if (pwm >= 0)
-  {
-    digitalWrite(RIGHT_FRONT_IN1, HIGH);
-    digitalWrite(RIGHT_FRONT_IN2, LOW);
-  }
-  else
-  {
-    digitalWrite(RIGHT_FRONT_IN1, LOW);
-    digitalWrite(RIGHT_FRONT_IN2, HIGH);
-    pwm = -pwm;
-  }
-  ledcWrite(RIGHT_FRONT_PWM, pwm);
-}
-
-// 右後車輪の制御
-void backRightMotor(double pwm)
-{
-  if (pwm >= 0)
-  {
-    digitalWrite(RIGHT_BACK_IN1, HIGH);
-    digitalWrite(RIGHT_BACK_IN2, LOW);
-  }
-  else
-  {
-    digitalWrite(RIGHT_BACK_IN1, LOW);
-    digitalWrite(RIGHT_BACK_IN2, HIGH);
-    pwm = -pwm;
-  }
-  ledcWrite(RIGHT_BACK_PWM, pwm);
 }
 
 void setup()
 {
   init_pwm_setup();
   
-  frontLeftMotor(0);
-  backLeftMotor(0);
-  frontRightMotor(0);
-  backRightMotor(0);
+  stopMotors();
 
   Serial.begin(115200);
   PS4.begin("1c:69:20:e6:20:d2");
@@ -133,80 +67,69 @@ void loop()
     if (PS4.Right())
     {
       Serial.println("Right");
-      frontLeftMotor(200);
-      backLeftMotor(-200);
-      frontRightMotor(-200);
-      backRightMotor(200);
-      delay(100);
+      driveMotor(0, 200);
+      driveMotor(1, -200);
+      driveMotor(2, -200);
+      driveMotor(3, 200);
     }
     if (PS4.Down())
     {
       Serial.println("Down");
-      frontLeftMotor(-200);
-      backLeftMotor(-200);
-      frontRightMotor(-200);
-      backRightMotor(-200);
-      delay(100);
+      driveMotor(0, -200);
+      driveMotor(1, -200);
+      driveMotor(2, -200);
+      driveMotor(3, -200);
     }
     if (PS4.Up())
     {
       Serial.println("Up");
-      frontLeftMotor(200);
-      backLeftMotor(200);
-      frontRightMotor(200);
-      backRightMotor(200);
-      delay(100);
+      driveMotor(0, 200);
+      driveMotor(1, 200);
+      driveMotor(2, 200);
+      driveMotor(3, 200);
     }
     if (PS4.Left())
     {
       Serial.println("Left");
-      frontLeftMotor(-200);
-      backLeftMotor(200);
-      frontRightMotor(200);
-      backRightMotor(-200);
-      delay(100);
+      driveMotor(0, -200);
+      driveMotor(1, 200);
+      driveMotor(2, 200);
+      driveMotor(3, -200);
     }
 
     if (PS4.UpRight())
     {
       Serial.println("Up Right");
-      frontLeftMotor(200);
-      backLeftMotor(0);
-      frontRightMotor(0);
-      backRightMotor(200);
-      delay(100);
+      driveMotor(0, 200);
+      driveMotor(1, 0);
+      driveMotor(2, 0);
+      driveMotor(3, 200);
     }
     if (PS4.DownRight())
     {
       Serial.println("Down Right");
-      frontLeftMotor(0);
-      backLeftMotor(-200);
-      frontRightMotor(-200);
-      backRightMotor(0);
-      delay(100);
+      driveMotor(0, 0);
+      driveMotor(1, -200);
+      driveMotor(2, -200);
+      driveMotor(3, 0);
     }
     if (PS4.UpLeft())
     {
       Serial.println("Up Left");
-      frontLeftMotor(0);
-      backLeftMotor(200);
-      frontRightMotor(200);
-      backRightMotor(0);
-      delay(100);
+      driveMotor(0, 0);
+      driveMotor(1, 200);
+      driveMotor(2, 200);
+      driveMotor(3, 0);
     }
     if (PS4.DownLeft())
     {
       Serial.println("Down Left");
-      frontLeftMotor(-200);
-      backLeftMotor(0);
-      frontRightMotor(0);
-      backRightMotor(-200);
-      delay(100);
+      driveMotor(0, -200);
+      driveMotor(1, 0);
+      driveMotor(2, 0);
+      driveMotor(3, -200);
     }
-
-    frontLeftMotor(0);
-    backLeftMotor(0);
-    frontRightMotor(0);
-    backRightMotor(0);
+    delay(100);
+    stopMotors();
   }
 }
